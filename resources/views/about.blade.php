43888ce2e98d05c5d5246577fcc0bc5c28ad042f
Alipmuh <m.aliffadilah@gmail.com>
2023-10-24 19:46:55 +0700

    modifikasi tulisan Halaman About pada about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('container')
-    <h1>Halaman Abouttt</h1>
+    <h1>Halaman About</h1>
     <h2>{{ $nama }}</h2>
     <p>{{ $email }}</p>
     <img src="img/{{ $gambar }}" alt="{{ $nama }}" width="200" class="img-thumbnaill rounded-circle">
