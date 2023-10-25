281f1220c142be17a537a30a95cb22f884b3e7c8
Alipmuh <m.aliffadilah@gmail.com>
2023-10-25 07:51:16 +0700

    nambah semua file di video 6.Database dan setengah yang 7.Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,32 +2,13 @@
 
 namespace App\Models;
 
-class Post 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Alif Fadilah",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum enim possimus, laboriosam molestias odit voluptatem cupiditate iste omnis doloremque tempora? Enim beatae nisi architecto asperiores! Minima quod assumenda repellat ducimus laboriosam totam nemo voluptatum animi aperiam nisi, sit, accusamus fugiat ratione necessitatibus ea! Error saepe reprehenderit itaque est impedit placeat vel praesentium ipsam molestias dolores quae, quibusdam atque nobis. Necessitatibus ipsa tempora, assumenda maxime minima, ut architecto beatae veritatis vitae amet nesciunt molestias totam, quis delectus animi tenetur eaque quisquam?"
-        ],
-        [
-            "title" => "Judul Post Alif Fadilah",
-            "slug" => "judul-post-kedua",
-            "author" => "Muhamad Alif",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis asperiores accusamus possimus assumenda quisquam ducimus architecto, consequuntur fugiat at ad illo magnam debitis, amet aspernatur quos quaerat temporibus expedita alias sapiente similique delectus. Veniam, corporis. Illo, explicabo reprehenderit. Sequi qui excepturi provident? Cupiditate officia, reiciendis labore eius laudantium assumenda animi quod aliquam minus deleniti illum recusandae sed quisquam similique corporis, natus et a harum, itaque dignissimos amet at eligendi molestiae. Culpa, suscipit accusantium maiores facere fugiat eaque et, iste facilis, velit recusandae praesentium. Autem molestias provident culpa, a quod consectetur magnam non incidunt aut impedit cumque nostrum doloremque deleniti pariatur?"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts -> firstWhere('slug', $slug);
-
-    }
+    protected $fillable=['title', 'excerpt', 'body'];
+    # protected $guarded=['id']; # agar id tidak bisa diisi 
 }
